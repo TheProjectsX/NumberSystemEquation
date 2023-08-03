c3e579c249788fce333f39407f7ad7e181813d2f
TheProjectsX <manvstech2.0@gmail.com>
2023-08-02 17:38:07 -0700

    #3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  reactStrictMode: true,
+
+  // Add basePath
+  basePath: "/NumberSystemEquation",
 };
 
 module.exports = nextConfig;
